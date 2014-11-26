9f38c973c8a2c4ac528606fde7f60215db157abb
puninvv <punin.v.v@gmail.com>
2014-11-26 22:38:37 +0400

    by Viktor
    добавил кнопочку, нажав на которую получаешь хэш файла, выбранного в
    диалоговом окне

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,12 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+/*by Punin Viktor*/
+#include <QMessageBox>
+#include <QFile>
+#include <QCryptographicHash>
+#include <QFileDialog>
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
@@ -11,4 +17,23 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_GetHashButton_clicked()
+{
+    QString FileDir = QFileDialog::getOpenFileName(this, tr("Open File"), "", tr("Files (*.*)"));
+
+
+    QFile file(FileDir);
+    QString hash;
+    if(file.open(QIODevice::ReadOnly))
+    {
+        hash = QCryptographicHash::hash(file.readAll(), QCryptographicHash::Md5).toHex().constData();
+        file.close();
+    }
+
+    QMessageBox msgBox;
+    msgBox.setText(hash);
+    msgBox.setDefaultButton(QMessageBox::Ok);
+    msgBox.exec();
 }
